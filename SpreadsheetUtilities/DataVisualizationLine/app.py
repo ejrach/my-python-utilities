5d8396a2625ed7de61c902c89ea4323ae624625e
ejrach <eric.rach@gmail.com>
2019-08-20 13:46:41 -0500

    Initial commit

---
+++ SpreadsheetUtilities/DataVisualizationLine/app.py
@@ -0,0 +1,38 @@
+# This script uses Pandas and Bokeh to provide a line graph based off large amounts of data.
+# Data is read in through a CSV file and plots X vs Y based off column titles.
+#
+# To install Pandas:    $ pip3 install pandas
+# To install Bokeh:     $ pip3 install bokeh
+
+# importing bokeh and pandas
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+import pandas
+
+# Define constants here
+CSV_INPUT = "data.csv"
+HTML_OUTPUT = "MyChart.html"
+X_AXIS_DATA_COLUMN = "Year"
+Y_AXIS_DATA_COLUMN = "Engineering"
+
+# prepare some data from csv
+df = pandas.read_csv(CSV_INPUT)
+x = df[X_AXIS_DATA_COLUMN]
+y = df[Y_AXIS_DATA_COLUMN]
+
+# prepare the output file
+output_file(HTML_OUTPUT)
+
+# create a figure object
+f = figure()
+
+# create line plot
+f.line(x, y)
+
+# triangle glyphs
+# f.triangle(x,y)
+
+# circle glyphs
+# f.circle(x,y)
+
+show(f)
